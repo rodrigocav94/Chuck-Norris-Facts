3ecf8803b2f9fbbb4e103e68ee81a32bfa7b5519
Rodrigo Alceu Cavalcanti <rodrigocav94@gmail.com>
2021-06-05 19:28:53 -0300

    Initial Commit

---
+++ Chuck Norris Facts/Chuck_Norris_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chuck_Norris_FactsApp.swift
+//  Chuck Norris Facts
+//
+//  Created by Rodrigo Cavalcanti on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Chuck_Norris_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chuck Norris FactsTests/Chuck_Norris_FactsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Chuck_Norris_FactsTests.swift
+//  Chuck Norris FactsTests
+//
+//  Created by Rodrigo Cavalcanti on 05/06/21.
+//
+
+import XCTest
+@testable import Chuck_Norris_Facts
+
+class Chuck_Norris_FactsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
